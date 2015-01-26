abbea2528e71a86c8f581942aa2815009c6b7040
SAIED <s.noroozi.cs@gamil.com>
2015-01-27 02:17:37 +0330

    changes related to fix performance issue on AMQP connection and channel

--- src/lsq_service.erl
+++ src/lsq_service.erl
@@ -1,26 +1,29 @@
--module(lsq_service). -export([
-    init/1,
-    allowed_methods/2,
-    content_type_provided/2,
-    to_html/2
+-module(lsq_service). 
+
+-export([
+    init/1
+    ,allowed_methods/2
+    ,to_html/2
  ]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 -include_lib("amqp_client/include/amqp_client.hrl").
 
--spec init(list()) -> {ok, term()}.
 init([]) ->
-    {ok, undefined}.
+   {ok,undefine}.
+
 
 allowed_methods(ReqData,Context)->
-{ ['GET'],ReqData,Context }.
+    {['GET'],ReqData,Context}.
 
-content_type_provided(rd,ctx)->
-	{[{"text/html",to_html}],rd,ctx}.
+get_timestamp() ->
+   {Mega, Sec, Micro} = os:timestamp(),
+   (Mega*1000000 + Sec)*1000 + round(Micro/1000). 
 
--spec to_html(wrq:reqdata(), term()) -> {iodata(), wrq:reqdata(), term()}.
 to_html(ReqData, State) ->
-
+   io:fwrite("\n--->"),
+   io:write(get_timestamp()),
+   
    Level = string:to_lower(wrq:get_qs_value("level","",ReqData)),
    Msg = wrq:get_qs_value("msg","",ReqData),
 
@@ -33,18 +36,27 @@ to_html(ReqData, State) ->
    {ok, RMQ_user} = application:get_env(App, rmq_user),
    {ok, RMQ_pass} = application:get_env(App, rmq_pass),   
 
+   AmqpParams = #amqp_params_network {
+	username = RMQ_user,
+	password = RMQ_pass,
+	host = RMQ_host,
+	port = RMQ_port
+	},
+
 
    case Level == "info" orelse Level == "warn" orelse Level == "error" of
   	true -> 
-	   {ok, Connection} =
-        	amqp_connection:start(#amqp_params_network{
-							host = RMQ_host, 
-							port = RMQ_port,
-							username = list_to_binary(RMQ_user),
-							password = list_to_binary(RMQ_pass)
-							}),
+	  %% {ok, Connection} =
+      	  %%      amqp_connection:start(#amqp_params_network{
+	  %%						host = RMQ_host, 
+	  %%						port = RMQ_port,
+	  %%						username = RMQ_user,
+	  %%						password = RMQ_pass
+ 	  %%						}),
 
-	    {ok, Channel} = amqp_connection:open_channel(Connection),
+	    {ok, Channel} = 
+			rmqp_get_channel(AmqpParams),
+			%%amqp_connection:open_channel(Connection),
 
 	    amqp_channel:call(Channel, #'queue.declare'{queue = QName}),
 
@@ -52,14 +64,44 @@ to_html(ReqData, State) ->
                       #'basic.publish'{
                         exchange = <<"">>,
                         routing_key = QName},
-                      #amqp_msg{payload = Payload}),
+                      #amqp_msg{payload = Payload});
 
-	    ok = amqp_channel:close(Channel),
-	    ok = amqp_connection:close(Connection);
+	    %%ok = amqp_channel:close(Channel);
+	    %%ok = amqp_connection:close(Connection);
 	false -> 
 		false
     end,
 
+    io:fwrite("\n<---"),
+    io:write(get_timestamp()),
+
     {"<html><body>Log service is running.</body></html>",ReqData,State}.
 
 	
+rmqp_get_channel(AmqpParams)->
+
+   case maybe_new_pid({AmqpParams, connection},
+	   fun() -> amqp_connection:start(AmqpParams) end) of
+	{ok, Client} ->
+	    maybe_new_pid({AmqpParams, channel},
+	        fun() -> amqp_connection:open_channel(Client) end);
+	Error ->
+	    Error
+    end.
+   
+maybe_new_pid(Group, StartFun) ->
+	case pg2:get_closest_pid(Group) of
+	     {error, {no_such_group, _}} ->
+	          pg2:create(Group),
+	          maybe_new_pid(Group, StartFun);
+	     {error, {no_process, _}} ->
+	        case StartFun() of
+	          {ok, Pid} ->
+	            pg2:join(Group, Pid),
+	            {ok, Pid};
+ 	          Error ->
+	            Error
+		end;
+	   Pid ->
+	      {ok, Pid}
+	end.
--- src/lsq_sup.erl
+++ src/lsq_sup.erl
@@ -13,6 +13,10 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
+    %%PoolerSup = {pooler_sup, {pooler_sup, start_link, []},
+      %%       permanent, infinity, supervisor, [pooler_sup]},
+%%{ok, {{one_for_one, 5, 10}, [PoolerSup]}},
+
     Web = {webmachine_mochiweb,
            {webmachine_mochiweb, start, [lsq_config:web_config()]},
            permanent, 5000, worker, [mochiweb_socket_server]},
